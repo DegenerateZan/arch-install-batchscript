91a60006aad1e3cdc5c248d46aa1959eacad107d
Ahmad Fauzan <92843449+DegenerateZan@users.noreply.github.com>
2022-01-01 22:29:42 +0700

    Update archautoinstall.sh

--- archautoinstall.sh
+++ archautoinstall.sh
@@ -4,16 +4,35 @@
 timedatectl set-ntp true
 
 echo "WARNING!!! USE THE DOS/MBR IF YOUR BIOS DOES NOT SUPPORT UEFI, AND ONLY SUPPORT LEGACY MODE"
-echo "ALSO DONT FORGET TO TURN ON THE BOOTABLE FLAG!!!"
+echo "ALSO DONT FORGET TO TURN ON THE BOOTABLE FLAG!!! OTHERWISE YOUR SYSTEM WOULD NOT BOOT!!"
 fdisk /dev/sda
 
 mkfs.ext4 /dev/root_partition
-mkswap /dev/swap_partition
+
+#uncomment the line below me if you want to use the swap partition
+
+#mkswap /dev/swap_partition
 
 mount /dev/root_partition /mnt
 
 swapon /dev/swap_partition
 
-pacstrap /mnt base linux-zen linux-zen-headers nano gedit grub network-manager dhcpcd sudo htop
+pacstrap /mnt base linux-zen linux-zen-headers nano gedit grub network-manager dhcpcd sudo htop git
+
+genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+hwclock --systohc
+
+echo "uncoment one of your locale"
+nano /etc/locale.gen
+
+locale-gen
+
+cat /etc/locale.gen >> /etc/locale.conf
+
+echo "SET YOUR ROOT PASSWORD!!!"
+passwd
